3a5e9ba9a000259a36a6d5c2f1b921cfb6a624c6
Rafi <m.rafi@capgemini.com>
2020-10-07 16:13:33 +0530

    GetExchange

---
+++ src/main/java/com/exchange/springboot/GetExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exchange/springboot/GetExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
